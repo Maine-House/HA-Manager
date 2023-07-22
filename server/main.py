d2b2ddc05f4bb402d38e5d14661058c9df8d94e5
Dax Harris <matteovh@gmail.com>
2023-07-22 13:03:54 -0400

    Refactor API location

--- server/main.py
+++ server/main.py
@@ -2,7 +2,7 @@ from requests import post
 from dotenv import load_dotenv
 load_dotenv()
 import os
-from lib.ha_api import HomeAssistant
+from lib.api import HomeAssistant
 
 api = HomeAssistant(os.getenv("HA_ADDRESS"), os.getenv("HA_TOKEN"))
 print({k: v.state for k, v in api.entities().items()})
