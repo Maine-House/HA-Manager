e4ac01c094e30e77bf83dd2b050f6305c5b490ee
Dax Harris <matteovh@gmail.com>
2023-07-24 15:12:08 -0400

    Universal sidebar & expansion

--- ham-frontend/src/pages/layout/Layout.tsx
+++ ham-frontend/src/pages/layout/Layout.tsx
@@ -1,30 +1,75 @@
 import {
+    ActionIcon,
     AppShell,
     Avatar,
     Box,
+    Button,
     Group,
     Header,
     LoadingOverlay,
+    Paper,
+    Stack,
     Switch,
+    Text,
     Title,
+    Tooltip,
     useMantineTheme,
 } from "@mantine/core";
 import { RiHomeWifiFill } from "react-icons/ri";
-import { MdLightMode, MdDarkMode } from "react-icons/md";
+import {
+    MdLightMode,
+    MdDarkMode,
+    MdBarChart,
+    MdDashboard,
+    MdManageAccounts,
+    MdMap,
+    MdRule,
+    MdSettings,
+    MdChevronLeft,
+    MdChevronRight,
+} from "react-icons/md";
 import "./layout.scss";
 import { useColorMode } from "../../util/colorMode";
 import { useApi } from "../../util/api/func";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { AccountStatus, useAccount } from "../../util/api/account";
+
+function IndexLink({
+    icon,
+    title,
+    tooltip,
+    target,
+}: {
+    icon: ReactNode;
+    title: string;
+    tooltip: string;
+    target: string;
+}) {
+    const nav = useNavigate();
+    return (
+        <Tooltip label={tooltip} withArrow color="dark" position="right">
+            <Paper
+                radius="sm"
+                className="index-link"
+                onClick={() => nav(target)}
+                p="sm"
+            >
+                {icon}
+                <Text className="title">{title}</Text>
+            </Paper>
+        </Tooltip>
+    );
+}
 
 export function Layout() {
     const theme = useMantineTheme();
     const [mode, setMode] = useColorMode();
     const { config } = useApi();
-    const { account } = useAccount();
+    const { account, logout } = useAccount();
     const nav = useNavigate();
     const location = useLocation();
+    const [expanded, setExpanded] = useState<boolean>(true);
 
     useEffect(() => {
         if (config && !config.initialized && location.pathname !== "/setup") {
@@ -102,7 +147,88 @@ export function Layout() {
                 })}
             >
                 <Box className="content">
-                    <Outlet />
+                    <Box
+                        className={"options" + (expanded ? " expanded" : "")}
+                        p="sm"
+                    >
+                        <Stack className="pages" spacing={"sm"}>
+                            <IndexLink
+                                icon={<MdDashboard />}
+                                title="Dashboard"
+                                tooltip="Quick access to basic controls"
+                                target="/"
+                            />
+                            <IndexLink
+                                icon={<MdBarChart />}
+                                title="Data"
+                                tooltip="View current & historical sensor data"
+                                target="/data"
+                            />
+                            <IndexLink
+                                icon={<MdSettings />}
+                                title="Settings"
+                                tooltip="Home Assistant Manager settings"
+                                target="/settings"
+                            />
+                            <IndexLink
+                                icon={<MdManageAccounts />}
+                                title="Users"
+                                tooltip="Manage user accounts"
+                                target="/settings/users"
+                            />
+                            <IndexLink
+                                icon={<MdMap />}
+                                title="Areas"
+                                tooltip="Manage areas & associated entities"
+                                target="/settings/areas"
+                            />
+                            <IndexLink
+                                icon={<MdRule />}
+                                title="Rules"
+                                tooltip="Manage automation rules"
+                                target="/settings/rules"
+                            />
+                        </Stack>
+                        <Group
+                            className="account"
+                            spacing={"sm"}
+                            position="left"
+                        >
+                            <Avatar
+                                className="account-icon"
+                                variant={mode === "dark" ? "light" : "gradient"}
+                            >
+                                {account.status === AccountStatus.loggedIn &&
+                                    account.username[0].toUpperCase()}
+                            </Avatar>
+                            <Text className="account-name">
+                                {account.status === AccountStatus.loggedIn &&
+                                    account.username}
+                            </Text>
+                            <Button
+                                className="logout-button"
+                                onClick={() => logout()}
+                                size="md"
+                            >
+                                Log Out
+                            </Button>
+                            <ActionIcon
+                                variant="filled"
+                                className="sidebar-expansion"
+                                color={mode === "dark" ? "dark" : "gray.5"}
+                                onClick={() => setExpanded(!expanded)}
+                            >
+                                {expanded ? (
+                                    <MdChevronLeft size={20} />
+                                ) : (
+                                    <MdChevronRight size={20} />
+                                )}
+                            </ActionIcon>
+                        </Group>
+                    </Box>
+                    <Box className="page-content">
+                        <Outlet />
+                    </Box>
                 </Box>
             </AppShell>
         </Box>
